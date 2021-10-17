8deb9f250759b8f55056d22d12ac838894f8c3ed
Danil <totem7h@gmail.com>
2021-10-17 11:56:12 +0400

    2. Сокобан. Тестовое задание. [#75283 #216379]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -5,28 +5,32 @@ public class Win {
         return isWin(board);
     }
 
-    public static boolean monoHorizontal(int[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][0] == 1 && board[i][1] == 1 && board[i][2] == 1 && board[i][3] == 1 && board[i][4] == 1) {
-                return true;
+    public static boolean monoHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != 1) {
+                result = false;
+                break;
             }
         }
-        return false;
+        return result;
     }
 
-    public static boolean monoVertical(int[][] board) {
+    public static boolean monoVertical(int[][] board, int column) {
+        boolean result = true;
         for (int i = 0; i < board.length; i++) {
-            if (board[0][i] == 1 && board[1][i] == 1 && board[2][i] == 1 && board[3][i] == 1 && board[4][i] == 1) {
-                return true;
+            if (board[i][column] != 1) {
+                result = false;
+                break;
             }
         }
-        return false;
+        return result;
     }
 
     public static boolean isWin(int[][] board) {
         boolean result = false;
         for (int i = 0; i < board.length; i++) {
-            if ((board[i][i] == 1 && (monoHorizontal(board) || monoVertical(board)))) {
+            if ((board[i][i] == 1 && (monoHorizontal(board, i) || monoVertical(board, i)))) {
                 result = true;
                 break;
             }
