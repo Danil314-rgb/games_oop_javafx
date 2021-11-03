97c8d60866f8b6620f0dd6da36e0360adfd92d0c
Danil <totem7h@gmail.com>
2021-11-03 13:47:14 +0400

    2. Каркас шахматной доски [#792 #222241]

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -2,6 +2,7 @@ package ru.job4j.chess;
 
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
+
 import java.util.Arrays;
 
 public final class Logic {
@@ -21,11 +22,9 @@ public final class Logic {
     }
 
     private boolean free(Cell[] steps) throws OccupiedCellException {
-        for (int index = 0; index < figures.length; index++) {
-            for (int i = 0; i < steps.length; i++) {
-                if (figures[index].position().equals(steps[i])) {
-                    throw new OccupiedCellException();
-                }
+        for (Cell item : steps) {
+            if (figures[index - 1] != null && figures[index - 1].position().equals(item)) {
+                throw new OccupiedCellException();
             }
         }
         return true;
--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -7,17 +7,10 @@ import ru.job4j.chess.firuges.black.BishopBlack;
 
 public class LogicTest {
 
-    @Test (expected = ImpossibleMoveException.class)
+    @Test(expected = ImpossibleMoveException.class)
     public void moveImpossibleMoveException() throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.F8));
         logic.move(Cell.F8, Cell.F7);
-    }
-
-    @Test (expected = NullPointerException.class)
-    public void moveOccupiedCellException() throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
-        Logic logic = new Logic();
-        logic.add(new BishopBlack(Cell.F8));
-        logic.move(Cell.F8, Cell.D6);
     }
 }
